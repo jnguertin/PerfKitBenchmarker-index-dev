3923dcef96ac11cfdd4e27044421b47a43a5ba3f
Scott Lin <shuninglin@google.com>
2025-07-25 19:57:03 +0000

    refined CUJ1 benchmark

--- perfkitbenchmarker/linux_benchmarks/edw_create_index_benchmark.py
+++ perfkitbenchmarker/linux_benchmarks/edw_create_index_benchmark.py
@@ -12,9 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""TODO
+"""
 
 Run command:
+
+BigQuery:
 
 ./pkb.py \
 --cloud=GCP  \
@@ -24,18 +26,38 @@ Run command:
 --config_override=edw_create_index_benchmark.edw_service.cluster_identifier=p3rf-bq-search.search_index_dataset \
 --gcp_service_account=bigquery-testing-pkb@p3rf-bigquery-smallquery-slots.iam.gserviceaccount.com \
 --gcp_service_account_key_file=/home/shuninglin/p3rf-bq-search-050c6559ed66.json \
---edw_index_creation_query_dir=edw/bigquery/search_index/create_index \
---edw_index_deletion_query_dir=edw/bigquery/search_index/delete_index \
---edw_index_check_query_dir=edw/bigquery/search_index/check_index \
+--edw_index_creation_query_dir=edw/bigquery/search_index/CUJ1 \
+--edw_power_queries=verify_no_index_query,create_index_query,delete_index_query,check_index_coverage_query \
 --metadata=cloud:GCP \
 --project=p3rf-bq-search \
 --zones=us-central1-c 
+
+Snowflake:
+
+TODO: Add SF queries to query folder and set up SF tables.
+
+./pkb.py \
+--cloud=AWS \
+--benchmarks=edw_create_index_benchmark \
+--snowflake_client_interface=JDBC  \
+--config_override=edw_create_index_benchmark.edw_service.type=snowflake_aws \
+--config_override=edw_index_benchmark.edw_service.cluster_identifier= \
+--snowflake_database=SEARCH_INDEX \
+--snowflake_schema=INDEX_TEST \
+--snowflake_warehouse=XSMALL_TEST \
+--edw_index_creation_query_dir=edw/snowflake_aws/search_index/CUJ1 \
+--edw_power_queries=verify_no_index_query,create_index_query,delete_index_query,check_index_coverage_query \
+--metadata=cloud:AWS \
+--snowflake_jdbc_client_jar=/home/shuninglin/snowflake-jdbc-client-2.13-enterprise.jar \
+--machine_type=m4.large \
+--zones=us-west-2a
 """
 
 """Benchmark for creating an index in an EDW service."""
 
 import os
 import time
+import logging
 from absl import flags
 from perfkitbenchmarker import configs
 from perfkitbenchmarker import edw_service
@@ -57,20 +79,6 @@ edw_create_index_benchmark:
 
 flags.DEFINE_string(
     'edw_index_creation_query_dir',
-    '',
-    'Optional local directory containing all query files. '
-    'Can be absolute or relative to the executable.',
-)
-
-flags.DEFINE_string(
-    'edw_index_deletion_query_dir',
-    '',
-    'Optional local directory containing all query files. '
-    'Can be absolute or relative to the executable.',
-)
-
-flags.DEFINE_string(
-    'edw_index_check_query_dir',
     '',
     'Optional local directory containing all query files. '
     'Can be absolute or relative to the executable.',
@@ -99,14 +107,56 @@ def Prepare(benchmark_spec):
   )
   edw_service_instance.GetClientInterface().Prepare('edw_common')
 
-  query_location = os.path.join(FLAGS.edw_index_creation_query_dir, 'create_index_query')
-  vm.PushDataFile(query_location)
+  query_locations = [
+      os.path.join(FLAGS.edw_index_creation_query_dir, query)
+      for query in FLAGS.edw_power_queries.split(',')
+  ]
+  any(vm.PushDataFile(query_loc) for query_loc in query_locations)
 
-  query_location = os.path.join(FLAGS.edw_index_deletion_query_dir, 'delete_index_query')
-  vm.PushDataFile(query_location)
 
-  query_location = os.path.join(FLAGS.edw_index_check_query_dir, 'check_index_coverage_query')
-  vm.PushDataFile(query_location)
+# Check if there's no index already created. If so, issue a command to delete the index and keep
+# checking until index is deleted or timeout
+def ensure_no_index(client_interface):
+  start_time = time.time()
+  timeout = 30
+  while True:
+    time_elapsed = time.time() - start_time
+    if time_elapsed > timeout:
+      logging.error("Timed out waiting for index to be deleted.")
+      # TODO: how to stop the benchmark
+      break
+    _, metadata = client_interface.ExecuteQuery('verify_no_index_query')
+    if metadata and metadata.get('rows_returned', 0) > 0:
+      client_interface.ExecuteQuery('delete_index_query')
+    else:
+      break
+    time.sleep(1) 
+
+
+# Create the index
+def create_index(client_interface, results):
+  execution_time, metadata = client_interface.ExecuteQuery('create_index_query')
+  results.append(sample.Sample('search_index_creation_time', execution_time, 'seconds', metadata))
+
+
+# Check Index Coverage until it reaches 100, record the time of reaching 100.
+# Time out if it takes too long to reach 100
+def measure_building_time(client_interface, results):
+  start_time = time.time()
+  timeout = 120
+  while True:
+    _, metadata = client_interface.ExecuteQuery('check_index_coverage_query')
+    time_elapsed = time.time() - start_time
+    if metadata and metadata.get('rows_returned', 0) > 0:
+      results.append(sample.Sample('search_index_available_time', time_elapsed, 'seconds', metadata))
+      break
+    if time_elapsed > timeout:
+      logging.error("Timed out waiting for index to be fully created.")
+      # TODO: how to stop the benchmark
+      break
+    else:
+      time.sleep(1) 
+
 
 def Run(benchmark_spec):
   """Runs the benchmark and returns a list of samples.
@@ -121,31 +171,13 @@ def Run(benchmark_spec):
 
   edw_service_instance = benchmark_spec.edw_service
   client_interface = edw_service_instance.GetClientInterface()
-  vm = benchmark_spec.vms[0]
 
-  # Delete Index If Exists
-  execution_time, metadata = client_interface.ExecuteQuery('delete_index_query')
-  results.append(sample.Sample('search_index_deletion_time', execution_time, 'seconds', metadata))
+  ensure_no_index(client_interface)
 
-  execution_time, metadata = client_interface.ExecuteQuery('create_index_query')
-  results.append(sample.Sample('search_index_creation_time', execution_time, 'seconds', metadata))
+  create_index(client_interface, results)
 
-  # Check Index Coverage until the index has been created
-  start_time = time.time()
-  timeout = 120
-  while True:
-    execution_time, metadata = client_interface.ExecuteQuery('check_index_coverage_query')
-    time_elapsed = time.time() - start_time
-    if metadata and metadata.get('rows_returned', 0) > 0:
-      results.append(sample.Sample('search_index_available_time', time_elapsed, 'seconds', metadata))
-      break
-    if time_elapsed > timeout:
-      print("Timed out waiting for index to be fully created.")
-      break
-    else:
-      time.sleep(1) 
+  measure_building_time(client_interface, results)
 
-  
   return results
 
 
